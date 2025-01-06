bd0041fdbc2a653f5d6c26e1c7e3f368a66f8528
user2278 <121397497+user2278@users.noreply.github.com>
2025-01-06 01:08:00 +0100

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Birthday</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+AU+SA:wght@100..400&display=swap" rel="stylesheet">
+</head>
+<body>
+    <img id="pic1" src="pic1.jpg" alt="error">
+
+    <div class="parr">
+    <div class="par1">Another year it is, Well Happy Birthday Hajar, Wish you all the happiness in your life and I hope you
+         achieve all your dreams and goal and be happy and proude of what you have achieved, 
+         and thanks for being a Friend of mine. LOVE YOU MY LIL GIRLFRIEND</div>
+    <img id="pic2" src="pic1.jpg" alt="error">
+    </div>
+</body>
+</html>
